db6a9ebfb8cc030c58b3c4e69479fbcc17e5ce58
Stella Mainar <mainars09@alumni.hanover.edu>
2019-03-12 13:49:57 -0600

    Add tests for turn class

---
+++ test/turn_test.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/turn'
+require './lib/card'
+require 'pry'
+
+class TurnTest < Minitest::Test
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+  end
+
+  def test_it_exists
+    assert_instance_of Turn, @turn
+  end
+
+  def test_it_has_attributes
+    assert_equal @card, @turn.card
+    assert_equal "Juneau", @turn.guess
+  end
+
+  def test_it_can_check_if_the_guess_is_correct_or_incorrect
+    assert_equal true, @turn.correct?
+
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    assert_equal false, turn.correct?
+  end
+
+  def test_it_can_return_feedback_whether_the_guess_is_correct_or_incorrect
+    assert_equal "Correct!", @turn.feedback
+
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    
+    assert_equal "Incorrect.", turn.feedback
+  end
+
+end
