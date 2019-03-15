aa06a6837764cc70566d0131f0bf2d371e6204cb
Stella Mainar <mainars09@alumni.hanover.edu>
2019-03-14 20:11:39 -0600

    Refactor feedback method for Turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,10 +14,10 @@ class Turn
   end
 
   def feedback
-    if correct? == true
-      p "Correct!"
+    if correct?
+      "Correct!"
     else
-      p "Incorrect."
+      "Incorrect."
     end
   end
 
